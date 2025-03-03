12ed6c971e984f8b63cc0032c0fd2058aa7529ef
spoonCastle <adrielcolherinhas@gmail.com>
2025-03-03 20:34:28 -0300

    chore: fixed build

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { v4 as uuidv4 } from 'uuid';
+
+globalThis.crypto = {
+  randomUUID: () => uuidv4(),
+} as Crypto;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
