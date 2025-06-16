1b58690bb226c0f8726624ce91a4e7c89e03a378
adrielCastejon <adrielcolherinhas@gmail.com>
2025-06-16 15:10:36 -0300

    breaking-change: “Disse-lhe Jesus: Eu sou o caminho, a verdade e a vida.” João 14:6

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { v4 as uuidv4 } from 'uuid';
 
-globalThis.crypto = {
-  randomUUID: () => uuidv4(),
-} as Crypto;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3339);
