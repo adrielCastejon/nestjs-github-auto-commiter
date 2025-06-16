7fd15a7b20da28ceedf9d6b7ae5f190d9ce9ea49
adrielCastejon <adrielcolherinhas@gmail.com>
2025-06-16 15:17:07 -0300

    docs: 'Os olhos são a candeia do corpo. Se os seus olhos forem bons, todo o seu corpo será cheio de luz.' Mateus 6:22

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { v4 as uuidv4 } from 'uuid';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
